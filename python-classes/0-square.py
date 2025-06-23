a35507a8e683abb6b97c382d3e034e231edaa997
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-06-23 12:21:35 +1000

    OOP-Python 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """this class defines a square's size."""
+
+    def __init__(self, size):
+        """Initialize a new square with a given size."""
+        self.__size = size
