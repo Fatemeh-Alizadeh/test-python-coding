60f8a892df7ae02bd035e32258b40cb7206e9f07
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-03 11:31:36 +1000

    Classes 00

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,36 +3,14 @@
 This is 0-square.py.
 This class defines a square's size
 """
+
+
 class Square:
     """
     Defines a square.
     """
-
     def __init__(self, size):
         """
         initialize a new square with a given size.
         """
         self.__size = size
-
-    @property
-    def size(self):
-        """
-        Get the size of the square.
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, size):
-        """
-        Set the size of the square with validation.
-        Args:
-            value (int): The size value to set.
-            Raises:
-                TypeError: If value is not an integer.
-                ValueError: If value is less than 0.
-        """
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
