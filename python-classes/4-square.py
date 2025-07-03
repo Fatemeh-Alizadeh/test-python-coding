e3e2f2568ab2f0d42356f7e02d83f83a7b635651
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-03 13:37:48 +1000

    Classes 04

---
+++ python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Defines a square with size and a method to compute its area.
+"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size."""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+        Set the size of the square with proper validation.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the # square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print("#" * self.__size)
