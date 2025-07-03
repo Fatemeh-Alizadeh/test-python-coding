a182c85aa726b44a74c0edbeff0c9e9357c3a92c
Fatemeh-Alizadeh <fatemeh-alizadeh@outlook.com>
2025-07-03 14:47:42 +1000

    inheritance 00

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is exactly an instance of a_class (not a subclass), False otherwise.
+    """
     return type(obj) == a_class
